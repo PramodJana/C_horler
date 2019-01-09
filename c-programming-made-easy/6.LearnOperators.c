a704f11dd17ea546f505e529148a753e5f7f8652
PramodJana <Pramodjana395@gmail.com>
2019-01-09 18:33:24 +0530

    operators

---
+++ c-programming-made-easy/6.LearnOperators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter the first number:");
+	scanf("%d",&a);
+	printf("Enter the second number:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("Sum=%d\n",c);
+}
