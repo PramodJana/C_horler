9e99da4218fb7b78bef927ec6f56bc3f6aeb51e2
PramodJana <Pramodjana395@gmail.com>
2019-01-09 18:58:43 +0530

    operators

--- c-programming-made-easy/6.LearnOperators.c
+++ c-programming-made-easy/6.LearnOperators.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void main()
 {
-	int a,b,c;
-	printf("Enter the first number:");
-	scanf("%d",&a);
-	printf("Enter the second number:");
-	scanf("%d",&b);
-	c=a+b;
-	printf("Sum=%d\n",c);
+	int a=6,b=3;
+	printf("%d AND %d is %d\n",a,b,a&b);
+	printf("%d AND %d is %d\n",a,b,a|b);
+	printf("%d AND %d is %d\n",a,b,a^b);
+
+	printf("5<<2 = %d\n",5<<2);
+	printf("5>>2 = %d\n",5>>2);
 }
